1bc509614497334624dd76b55f103716ffec0849
suren-hov <shovhannisyan2021@gmail.com>
2024-08-14 15:19:51 +0400

    added arrays

---
+++ basic-syntax/arrays.js
@@ -0,0 +1,9 @@
+const fruits = ['apple', 'banana', 'cherry']
+
+console.log(fruits[0]) // Accessing array elements
+
+fruits.push('orange') // Adding an element to the array
+
+fruits.pop() // Removing the last element from the array
+
+console.log(fruits.length) // Getting the length of the array
