c713cdebe0f43acf4caa2af5a4078a082eb4914f
suren-hov <shovhannisyan2021@gmail.com>
2024-08-14 14:56:59 +0400

    Added first two basics

---
+++ basic-syntax/data-types.js
@@ -0,0 +1,31 @@
+// String
+const name = 'John'
+
+// Number
+const age = 25
+
+// Boolean
+const isStudent = true
+
+// Undefined
+let undefinedValue
+
+// Null
+const emptyValue = null
+
+// Object
+const person = {
+  firstName: 'Jane',
+  lastName: 'Doe'
+}
+
+// Array
+const colors = ['red', 'green', 'blue']
+
+console.log(name)
+console.log(age)
+console.log(isStudent)
+console.log(undefinedValue)
+console.log(emptyValue)
+console.log(person.firstName)
+console.log(colors[0])
---
+++ basic-syntax/variables.js
@@ -0,0 +1,13 @@
+// Using var (old way)
+var name = 'John'
+
+// Using let (block-scoped)
+let age = 30
+age = 35
+
+// Using const (constant value)
+const isStudent = true
+
+console.log(name)
+console.log(age)
+console.log(isStudent)
