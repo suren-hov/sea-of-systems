c099bbe8788c76224abe4437d83daf4b88d0bf80
suren-hov <shovhannisyan2021@gmail.com>
2024-08-14 15:15:43 +0400

    added loops

---
+++ basic-syntax/loops.js
@@ -0,0 +1,25 @@
+// For Loop
+
+for (let i = 0; i < 5; i++) {
+  console.log('Iteration:', i)
+}
+
+// While Loop
+
+let i = 0
+
+while (i < 5) {
+  console.log('While Iteration:', i)
+
+  i++
+}
+
+// Do-While Loop
+
+let j = 0
+
+do {
+  console.log('Do-While Iteration:', j)
+
+  j++
+} while (j < 5)
