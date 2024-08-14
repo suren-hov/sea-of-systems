50cb8f3cf6a573a9a096fc4b2c9cad417eaae520
suren-hov <shovhannisyan2021@gmail.com>
2024-08-14 15:20:51 +0400

    Added string methods

---
+++ basic-syntax/string-methods.js
@@ -0,0 +1,11 @@
+const text = 'Hello, World!'
+
+console.log(text.length) // Length of the string
+
+console.log(text.toUpperCase()) // Convert to uppercase
+
+console.log(text.toLowerCase()) // Convert to lowercase
+
+console.log(text.indexOf('World')) // Find the position of a substring
+
+console.log(text.slice(0, 5)) // Extract a part of the string
